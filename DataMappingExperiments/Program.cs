6532845b8e4ae2d945593e8ea6216c53815defac
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-03-21 10:45:04 +0100

    Initial Test commit

--- DataMappingExperiments/Program.cs
+++ DataMappingExperiments/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using XmlSchema = System.Xml.Schema.XmlSchema;
 
 namespace DataMappingExperiments
 {
@@ -10,6 +9,39 @@ namespace DataMappingExperiments
   {
     static void Main(string[] args)
     {
+      string fileName =
+        @"c:\users\fresan\documents\visual studio 2015\Projects\DataMappingExperiments\DataMappingExperiments\Testdata\Fln-Blg Plattformar Enkel.xlsx";
+      string xsd = @"C:\Users\fresan\documents\visual studio 2015\Projects\DataMappingExperiments\DataMappingExperiments\Testdata\ANDAImport.xsd";
+      //TODO: 1. Take a excel file input
+      StartExcelManager(fileName);
+      //TODO: 2. Map the data from the excel against a class
+      //TODO: 3. Output data into a XML format
+      Console.ReadLine();
+    }
+
+    //https://code.msdn.microsoft.com/office/How-to-convert-excel-file-7a9bb404
+    //https://github.com/ExcelDataReader/ExcelDataReader
+    //https://www.codeproject.com/articles/10581/convert-excel-to-xml-file-xml-schema-and-validate
+    //https://coderwall.com/p/app3ya/read-excel-file-in-c
+
+    static void StartExcelManager(string fileName)
+    {
+      if (string.IsNullOrEmpty(fileName) || !File.Exists(fileName))
+      {
+        Console.WriteLine("The file is invalid. Select a valid file.");  
+        return;
+      }
+      try
+      {
+        var excelManager = new ExcelManager();
+        excelManager.ExcelConversion(fileName);
+      }
+      catch (Exception)
+      {
+        
+        throw;
+      }
+      ;
     }
   }
 }
