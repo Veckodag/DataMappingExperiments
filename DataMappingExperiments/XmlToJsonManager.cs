24962aec9313306b7eb05f093a04580b3cffef9f
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-06-30 13:48:55 +0200

    Preparing XmlToJson

---
+++ DataMappingExperiments/XmlToJsonManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Office.Interop.Excel;
+
+namespace DataMappingExperiments
+{
+  public class XmlToJsonManager
+  {
+
+  }
+}
